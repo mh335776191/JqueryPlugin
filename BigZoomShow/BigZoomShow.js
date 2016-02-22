d51b947c22303e26712d489faf19c0bcee87738e
马欢公司 <418394282@qq.com>
2016-02-22 09:54:33 +0800

    提交

--- BigZoomShow/BigZoomShow.js
+++ BigZoomShow/BigZoomShow.js
@@ -7,7 +7,7 @@
     $.fn.BigShow = function (options) {
         var opt = $.extend({}, defaults, options);
         var zoomdiv = '<div style="height:' + $(document).height() + ';widht' + $(document).width() + '; position: absolute; top:0px; filter: alpha(opacity=60);background-color: #777;z-index: 1002; left: 0px;opacity:0.5; -moz-opacity:0.5; "></div>';
-        $(document).append(zoomdiv);
+        $(document.body).append(zoomdiv);
         $(zoomdiv).show();
         var bigtemp = $(this).copy();
         bigtemp.css("font-size", opt.fontsize);
